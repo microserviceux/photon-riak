f153c5accf0dd9fa9ceb34f115476b817634cce9
Sergio Alvarez-Napagao <tranchis@gmail.com>
2015-10-22 16:07:57 +0100

    Fixed problems with Java code

---
+++ doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to photon-riak
+
+TODO: write [great documentation](http://jacobian.org/writing/what-to-write/)
---
+++ project.clj
@@ -0,0 +1,14 @@
+(defproject tranchis/photon-riak "0.9.3"
+  :description "FIXME: write description"
+  :url "http://example.com/FIXME"
+  :license {:name "Eclipse Public License"
+            :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :java-source-paths ["java"]
+  :aot :all
+  :dependencies [[org.clojure/clojure "1.7.0"]
+                 [tranchis/photon-db "0.9.3"]
+                 [tranchis/photon-config "0.9.3"]
+                 [clj-http "1.1.2"]
+                 [clj-time "0.11.0"]
+                 [com.basho.riak/riak-client "2.0.1"
+                  :exclusions [com.sun/tools]]])
